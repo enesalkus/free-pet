8c8f7563de7b45757a3c22ade4da10537c8d7de4
Enes <enesalkus@hotmail.com>
2021-02-28 11:37:07 +0300

    Update

--- FreePet/FreePet/Form1.cs
+++ FreePet/FreePet/Form1.cs
@@ -19,7 +19,7 @@ namespace FreePet
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("deneme");
+            MessageBox.Show("asd");
         }
     }
 }
